317b0846332f70faf58ee7c69424c4973618ee5f
George Atkinson <georgeatkinsonwd@gmail.com>
2023-02-01 15:34:28 +0000

    working CRUD backend

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
+
 
 
 
@@ -7,7 +9,7 @@ const Goal = require('../models/goalModel')
 // get goals
 // route GET /api/goals
 const getGoals = asyncHandler(async (req,res) => {
-   const goals = await Goal.find()
+   const goals = await Goal.find({user: req.user.id})
 
    res.status(200).json(goals)
 })
@@ -20,7 +22,8 @@ const setGoal = asyncHandler(async (req,res) => {
         throw new Error('Please add a text field')
     }
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
     res.status(200).json(goal)
 })
@@ -35,6 +38,20 @@ const updateGoal = asyncHandler(async (req,res) => {
         throw new Error('Goal not found')
     }
 
+
+    const user = await User.findById(req.user.id)
+    // check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+
+    //make sure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+    }
 
     // creates if doesn't exist
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
@@ -52,6 +69,20 @@ const deleteGoal = asyncHandler(async (req,res) => {
     if(!goal){
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+    // check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+
+    //make sure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
     }
     await goal.remove()
 
