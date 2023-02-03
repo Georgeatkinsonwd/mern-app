3014cc054b927aca01dd66e499869b9247953a8e
George Atkinson <georgeatkinsonwd@gmail.com>
2023-02-03 18:06:47 +0000

    added some redux

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,54 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import authService from './authService'
+
+
+// GET user from localStorage
+
+const user = JSON.parse(localStorage.getItem('user'))
+
+const initialState = {
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    isLoding: false,
+    message: '',
+}
+
+// Register the User
+
+export const register = createAsyncThunk('auth/register', async (user,thunkAPI)=>{
+    try {
+        return await authService.register(user)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+
+
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        reset: (state) => {
+            state.isLoding = false
+            state.isSuccess = false
+            state.isError = false
+            state.message = ''
+        },
+    },
+    extraReducers: () => {}
+})
+
+
+
+
+
+
+
+
+
+export const {reset} = authSlice.actions
+export default authSlice.reducer
